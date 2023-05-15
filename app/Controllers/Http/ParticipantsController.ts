c648b8a68a7aa8265a3278ead9bbb7c30844594e
david keukeu <davidkeukeu8@gmail.com>
2023-05-16 01:25:37 +0200

    fix some bugs

--- app/Controllers/Http/ParticipantsController.ts
+++ app/Controllers/Http/ParticipantsController.ts
@@ -23,4 +23,27 @@ export default class ParticipantsController {
         }
     }
 
+    public async delete({request,response}:HttpContextContract){
+        try{
+        const {id} = request.param('id')
+            const participant = await Participant.find(id)
+            await participant?.delete()
+            if( !participant ) {
+                return response.notFound('Unavailable participant')
+            }else{
+                return response.ok(`${participant.name} deleted with succes`)
+            }
+        }catch(e){
+            throw new Error(e)
+        }
+    }
+
+    public async update({request,response}:HttpContextContract) {
+        const {id} = request.param('id')
+        const payload = await request.validate(ParticipantValidator)
+        try{
+            const participantFing = await Participant.find(id)
+        }
+    }
+
 }
